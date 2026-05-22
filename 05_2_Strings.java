d1e00f8586b99965e7c66debae375a741d74c599
Shaurya002800 <kunwarshaurya28@gmail.com>
2026-05-22 11:27:58 +0530

    roman to integer

--- 05_2_Strings.java
+++ 05_2_Strings.java
@@ -14,34 +14,85 @@
 
 
 
+// class Solution {
+//     public String frequencySort(String s) {
+//         char[] str = s.toCharArray();
+//         int[] freq = new int[128];
+//         for (int i = 0; i < str.length; i++) {
+//             freq[str[i]]++;
+//         }
+//         for (int i = 0; i < str.length;) {
+//             int maxFreq = 0;
+//             char cmax = 0;
+//             for (int j = 0; j < 128; j++) {
+//                 if (freq[j] > maxFreq) {
+//                     maxFreq = freq[j];
+//                     cmax = (char) j;
+//                 }
+//             }
+//             while (freq[cmax] != 0) {
+//                 str[i++] = cmax;
+//                 freq[cmax]--;
+//             }
+//         }
+=//         return new String(str);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+// Symbol       Value
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+// For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+// Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+// I can be placed before V (5) and X (10) to make 4 and 9. 
+// X can be placed before L (50) and C (100) to make 40 and 90. 
+// C can be placed before D (500) and M (1000) to make 400 and 900.
+// Given a roman numeral, convert it to an integer.
+
+
+
+
+
 class Solution {
-    public String frequencySort(String s) {
-        // Convert input string to character array
-        char[] str = s.toCharArray();
-        // Create an array to store frequency of each character
-        int[] freq = new int[128];
-        // Count the frequency of each character in the input string
-        for (int i = 0; i < str.length; i++) {
-            freq[str[i]]++;
-        }
-        // Sort the characters based on their frequency
-        for (int i = 0; i < str.length;) {
-            int maxFreq = 0;
-            char cmax = 0;
-            // Find the character with the maximum frequency
-            for (int j = 0; j < 128; j++) {
-                if (freq[j] > maxFreq) {
-                    maxFreq = freq[j];
-                    cmax = (char) j;
-                }
+    public int romanToInt(String s) {
+        int ans = 0;
+        for(int i = 0; i < s.length(); i++) {
+            int curr = value(s.charAt(i));
+            if(i < s.length() - 1 &&
+               curr < value(s.charAt(i + 1))) {
+                ans -= curr;
             }
-            // Append the characters to the result string based on their frequency
-            while (freq[cmax] != 0) {
-                str[i++] = cmax;
-                freq[cmax]--;
+            else {
+                ans += curr;
             }
         }
-        // Convert the character array back to a string and return it
-        return new String(str);
+        return ans;
+    }
+    public int value(char ch) {
+        if(ch == 'I') return 1;
+        if(ch == 'V') return 5;
+        if(ch == 'X') return 10;
+        if(ch == 'L') return 50;
+        if(ch == 'C') return 100;
+        if(ch == 'D') return 500;
+        return 1000;
     }
 }
