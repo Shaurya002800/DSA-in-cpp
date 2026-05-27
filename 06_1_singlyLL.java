d0cb7720da4ba28eda00a0003fa75531def13294
Shaurya002800 <kunwarshaurya28@gmail.com>
2026-05-27 11:57:21 +0530

    detect the cycle

--- 06_1_singlyLL.java
+++ 06_1_singlyLL.java
@@ -1,3 +1,13 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+
+
 // There is a singly-linked list head and we want to delete a node node in it.
 
 // You are given the node to be deleted node. You will not be given access to the first node of head.
@@ -101,28 +111,65 @@
 /**
  * Definition for singly-linked list.
  */
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
 
-class Solution {
-    public ListNode reverseList(ListNode head) {
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
         
-        ListNode prev = null;
-        ListNode curr = head;
+//         ListNode prev = null;
+//         ListNode curr = head;
 
-        while (curr != null) {
-            ListNode nextTemp = curr.next; // store next node
-            curr.next = prev;              // reverse link
-            prev = curr;                   // move prev
-            curr = nextTemp;               // move curr
+//         while (curr != null) {
+//             ListNode nextTemp = curr.next; // store next node
+//             curr.next = prev;              // reverse link
+//             prev = curr;                   // move prev
+//             curr = nextTemp;               // move curr
+//         }
+
+//         return prev;
+//     }
+
+// }
+
+
+
+
+
+
+
+
+
+
+// given head, the head of a linked list, determine if the linked list has a cycle in it.
+// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+// Return true if there is a cycle in the linked list. Otherwise, return false.
+
+ 
+// Example 1:
+
+// Input: head = [3,2,0,-4], pos = 1
+// Output: true
+// Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+
+// Floyd Cycle Detection Algorithm
+
+// Also called:
+
+// Tortoise and Hare Algorithm
+
+
+public class 06_1_singlyLL {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            // cycle detected
+            if(slow == fast) {
+                return true;
+            }
         }
-
-        return prev;
+        return false;
     }
-
 }
