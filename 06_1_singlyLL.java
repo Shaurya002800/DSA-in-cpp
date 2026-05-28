e7d2d1b13bb2e69388bfb419d385f6ea02b80554
Shaurya002800 <kunwarshaurya28@gmail.com>
2026-05-28 14:53:51 +0530

    odd even ll

--- 06_1_singlyLL.java
+++ 06_1_singlyLL.java
@@ -189,37 +189,82 @@ class ListNode {
 // Output: true
 
 
+// class Solution {
+//     public boolean isPalindrome(ListNode head) {
+//         if(head == null || head.next == null){
+//             return true;
+//         }
+//         ListNode slow = head;
+//         ListNode fast = head;
+//         // Find middle
+//         while(fast != null && fast.next != null){
+//             slow = slow.next;
+//             fast = fast.next.next;
+//         }
+//         // Reverse second half
+//         ListNode prev = null;
+//         ListNode curr = slow;
+//         while(curr != null){
+//             ListNode next = curr.next;
+//             curr.next = prev;
+//             prev = curr;
+//             curr = next;
+//         }
+//         // Compare both halves
+//         ListNode first = head;
+//         ListNode second = prev;
+//         while(second != null){
+//             if(first.val != second.val){
+//                 return false;
+//             }
+//             first = first.next;
+//             second = second.next;
+//         }
+//         return true;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//odd even linked list
+
+// Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
+// The first node is considered odd, and the second node is even, and so on.
+// Note that the relative order inside both the even and odd groups should remain as it was in the input.
+// You must solve the problem in O(1) extra space complexity and O(n) time complexity.
+
+
+// Example 1:
+// Input: head = [1,2,3,4,5]
+// Output: [1,3,5,2,4]
+
+
 class Solution {
-    public boolean isPalindrome(ListNode head) {
+    public ListNode oddEvenList(ListNode head) {
         if(head == null || head.next == null){
-            return true;
+            return head;
         }
-        ListNode slow = head;
-        ListNode fast = head;
-        // Find middle
-        while(fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+        while(even != null && even.next != null){
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
         }
-        // Reverse second half
-        ListNode prev = null;
-        ListNode curr = slow;
-        while(curr != null){
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        // Compare both halves
-        ListNode first = head;
-        ListNode second = prev;
-        while(second != null){
-            if(first.val != second.val){
-                return false;
-            }
-            first = first.next;
-            second = second.next;
-        }
-        return true;
+        odd.next = evenHead;
+        return head;
     }
 }
