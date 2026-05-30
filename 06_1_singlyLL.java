38cf4dad731a792acd567f4445e73560adede0d0
Shaurya002800 <kunwarshaurya28@gmail.com>
2026-05-30 12:41:41 +0530

    intersection of two ll

--- 06_1_singlyLL.java
+++ 06_1_singlyLL.java
@@ -333,46 +333,119 @@ class ListNode {
 
 
 
+// class Solution {
+//     public ListNode sortList(ListNode head) {
+//         if(head == null || head.next == null) {
+//             return head;
+//         }
+//         // Find middle
+//         ListNode slow = head;
+//         ListNode fast = head.next;
+//         while(fast != null && fast.next != null) {
+//             slow = slow.next;
+//             fast = fast.next.next;
+//         }
+//         ListNode mid = slow.next;
+//         slow.next = null;
+//         // Sort left and right halves
+//         ListNode left = sortList(head);
+//         ListNode right = sortList(mid);
+//         // Merge sorted halves
+//         return merge(left, right);
+//     }
+//     public ListNode merge(ListNode l1, ListNode l2) {
+//         ListNode dummy = new ListNode(0);
+//         ListNode tail = dummy;
+//         while(l1 != null && l2 != null) {
+//             if(l1.val <= l2.val) {
+//                 tail.next = l1;
+//                 l1 = l1.next;
+//             }
+//             else {
+//                 tail.next = l2;
+//                 l2 = l2.next;
+//             }
+//             tail = tail.next;
+//         }
+//         if(l1 != null) {
+//             tail.next = l1;
+//         }
+//         if(l2 != null) {
+//             tail.next = l2;
+//         }
+//         return dummy.next;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// INTERSECTION OF TWO LINKED LISTS
+
+// Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+
+// For example, the following two linked lists begin to intersect at node c1:
+
+
+// The test cases are generated such that there are no cycles anywhere in the entire linked structure.
+
+// Note that the linked lists must retain their original structure after the function returns.
+
+// Custom Judge:
+
+// The inputs to the judge are given as follows (your program is not given these inputs):
+
+// intersectVal - The value of the node where the intersection occurs. This is 0 if there is no intersected node.
+// listA - The first linked list.
+// listB - The second linked list.
+// skipA - The number of nodes to skip ahead in listA (starting from the head) to get to the intersected node.
+// skipB - The number of nodes to skip ahead in listB (starting from the head) to get to the intersected node.
+// The judge will then create the linked structure based on these inputs and pass the two heads, headA and headB to your program. If you correctly return the intersected node, then your solution will be accepted.
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 class Solution {
-    public ListNode sortList(ListNode head) {
-        if(head == null || head.next == null) {
-            return head;
-        }
-        // Find middle
-        ListNode slow = head;
-        ListNode fast = head.next;
-        while(fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        ListNode mid = slow.next;
-        slow.next = null;
-        // Sort left and right halves
-        ListNode left = sortList(head);
-        ListNode right = sortList(mid);
-        // Merge sorted halves
-        return merge(left, right);
-    }
-    public ListNode merge(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-        ListNode tail = dummy;
-        while(l1 != null && l2 != null) {
-            if(l1.val <= l2.val) {
-                tail.next = l1;
-                l1 = l1.next;
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA;
+        ListNode b = headB;
+
+        while(a != b) {
+
+            if(a == null) {
+                a = headB;
             }
             else {
-                tail.next = l2;
-                l2 = l2.next;
+                a = a.next;
             }
-            tail = tail.next;
+
+            if(b == null) {
+                b = headA;
+            }
+            else {
+                b = b.next;
+            }
         }
-        if(l1 != null) {
-            tail.next = l1;
-        }
-        if(l2 != null) {
-            tail.next = l2;
-        }
-        return dummy.next;
+        return a;
     }
 }
