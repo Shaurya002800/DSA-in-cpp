5b29dc98d98cf6629a5ecd8c7d76c8b80365edb1
Shaurya002800 <kunwarshaurya28@gmail.com>
2026-06-04 13:57:34 +0530

    count good numbers

--- 07_recursion.java
+++ 07_recursion.java
@@ -84,20 +84,72 @@
 
 
 
- class Solution {
-    public double myPow(double x, int n) {
-        long N = n;
-        if(N < 0) {
-            x = 1 / x;
-            N = -N;
-        }
-        double ans = 1;
-        while(N > 0) {
-            if(N % 2 == 1) {
-                ans *= x;
+//  class Solution {
+//     public double myPow(double x, int n) {
+//         long N = n;
+//         if(N < 0) {
+//             x = 1 / x;
+//             N = -N;
+//         }
+//         double ans = 1;
+//         while(N > 0) {
+//             if(N % 2 == 1) {
+//                 ans *= x;
+//             }
+//             x *= x;
+//             N /= 2;
+//         }
+//         return ans;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//COUNT GOOD NuMBERS
+
+// A digit string is good if the digits (0-indexed) at even indices are even and the digits at odd indices are prime (2, 3, 5, or 7).
+// For example, "2582" is good because the digits (2 and 8) at even positions are even and the digits (5 and 2) at odd positions are prime. However, "3245" is not good because 3 is at an even index but is not even.
+// Given an integer n, return the total number of good digit strings of length n. Since the answer may be large, return it modulo 109 + 7.
+// A digit string is a string consisting of digits 0 through 9 that may contain leading zeros.
+
+// Example 1:
+
+// Input: n = 1
+// Output: 5
+// Explanation: The good numbers of length 1 are "0", "2", "4", "6", "8".
+
+
+
+class Solution {
+    long MOD = 1000000007;
+    public int countGoodNumbers(long n) {
+        long even = (n + 1) / 2;
+        long odd = n / 2;
+        long ans = (power(5, even) * power(4, odd)) % MOD;
+        return (int)ans;
+    }
+    public long power(long x, long n) {
+        long ans = 1;
+        while(n > 0) {
+            if((n & 1) == 1) {
+                ans = (ans * x) % MOD;
             }
-            x *= x;
-            N /= 2;
+            x = (x * x) % MOD;
+            n >>= 1;
         }
         return ans;
     }
